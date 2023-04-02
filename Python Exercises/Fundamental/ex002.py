d50df39c07b98b7fee5c19f66588702d49cf4ca2
Rafael-Leandro <rafael.leandro25@hotmail.com>
2023-04-02 15:12:02 -0300

    Create ex002.py
    Second Python exercise

---
+++ Python Exercises/Fundamental/ex002.py
@@ -0,0 +1,6 @@
+'''Write a program that reads a person's name and displays a welcome message'''
+name = input('Write your name: ')
+print('Nice to meet you ', name)
+
+'''Another way to print the message'''
+print('Nice to meet you, {}!'.format(name))
