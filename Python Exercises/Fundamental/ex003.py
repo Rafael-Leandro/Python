c3785a9df10b73e15f9fc4395b9df1635f9c1415
Rafael-Leandro <rafael.leandro25@hotmail.com>
2023-04-15 15:48:42 -0300

    Create ex003.py
    Third Python exercise

---
+++ Python Exercises/Fundamental/ex003.py
@@ -0,0 +1,5 @@
+'''Write a program that reads two numbers and prints the sum of them'''
+n1 = int(input('Write a number: '))
+n2 = int(input('Write another number: '))
+s = n1 + n2
+print('The sum of {} and {} is equal to {}.'.format(n1, n2, s))
