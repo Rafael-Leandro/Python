6c51a29f0ec4230773f7b864eede60edf0c62ffe
Rafael-Leandro <rafael.leandro25@hotmail.com>
2023-06-01 15:53:22 -0300

    Create ex007.py
    Seventh Python exercise

---
+++ Python Exercises/Fundamental/ex007.py
@@ -0,0 +1,5 @@
+'''Write a program that reads two grades of a student, calculates it and displays it's average'''
+grade1 = float(input("First student's grade: "))
+grade2 = float(input("Second student's grade: "))
+average = (grade1 + grade2) / 2
+print('The average between {:.1f} and {:.1f} is equal to {:.1f}'.format(grade1, grade2, average))
